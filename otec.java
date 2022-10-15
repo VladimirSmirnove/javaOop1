0e1cb87ef9f64711a540288854f3061d0cb9b11b
Vladimir <vladsmir323@gmail.com>
2022-10-15 08:58:17 +0300

    Наследник первый

---
+++ otec.java
@@ -0,0 +1,10 @@
+public class otec extends ded {
+        public otec (String name, int age) {
+            super(name, age);
+        }
+        @Override
+        public void Person() {
+            System.out.println( toString() + "Я сын и отец");
+            
+    }
+}
