e18724b4fe4c407435ed55940f276ce76abb0ed4
Vladimir <vladsmir323@gmail.com>
2022-10-15 08:58:43 +0300

    Наследник второй

---
+++ sun.java
@@ -0,0 +1,15 @@
+
+public class sun extends ded {
+        public sun (String name, int age) {
+            super (name, age);
+            
+        }
+        @Override
+        public void Person() {
+            
+            System.out.println(toString() + "Я сын и внук");
+            
+        }
+        
+    }
+
